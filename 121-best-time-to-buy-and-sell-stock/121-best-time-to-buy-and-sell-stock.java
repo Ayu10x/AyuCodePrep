8ec574f0a12182c9a993d05441bb46df6d2a9c76
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2022-08-05 21:16:37 +0530

    Time: 3 ms (69.64%), Space: 75.6 MB (84.73%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int sol=0;
-        if(prices ==null || prices.length <=1) return sol;
+        if(prices == null || prices.length <= 1) return sol;
         
-        int min  = prices[0];
+        int min = prices[0];
         for(int i=1; i<prices.length; i++){
             if(prices[i] > min){
                 sol = Math.max(sol, prices[i] - min);
