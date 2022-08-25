555ae3359f336d25a573a6c1af9c7e8122f5bf67
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2022-08-25 11:24:45 +0530

    Time: 3 ms (92.28%), Space: 46.6 MB (53.26%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote == null){ return true;}
-        if(magazine == null){return false;}
+        if(ransomNote == null) {return false;}
+        if(magazine == null) {return false;}
         
-        if(magazine.length() < ransomNote.length()){ return false;}
+        if(ransomNote.length()  >  magazine.length()) {return false;}
         
-        int[] count =  new int[26];
-        for(char c: magazine.toCharArray()){
-            count[c - 'a']++;
-            }
-        for(char c: ransomNote.toCharArray()){
-            if(count[c - 'a'] == 0){ return false;}
-    count[c - 'a'] -= 1   ; 
-    }
+       int[] freq = new int[26];
+        for(char c : magazine.toCharArray()){
+            freq[c - 'a'] += 1;
+        }
+        for(char c : ransomNote.toCharArray()){
+            if(freq[c - 'a'] == 0){ return false; }
+            freq[c - 'a'] -= 1;
+        }
         return true;
     }
 }
