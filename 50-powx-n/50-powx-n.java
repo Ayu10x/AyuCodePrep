a5ab41c1586d15a67d4d60809ed8ee8e4c4a8dbb
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2022-08-27 11:07:51 +0530

    Time: 1 ms (89.85%), Space: 42.3 MB (77.32%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,6 +1,40 @@
 class Solution {
     public double myPow(double x, int n) {
-      double res =  Math.pow(x,n);
-      return res;
+        
+    //Fuddu Code using Math Function:
+        // double res =  Math.pow(x,n);
+        // return res;
+        
+        //Recursion1
+        //: This code runs for TC: o(n)
+        //Below the solution divides the problem into subproblems
+        // of size y/2 and call the subproblems recursively.
+        
+        //BaseCase
+        // if(n == 0) {return 1;}
+        // else if(n % 2 == 0){
+        //     return myPow(x, n/2) * myPow(x, n/2);
+        // }else{
+        //     return x * myPow(x, n/2) * myPow(x, n/2);
+        // }
+        
+        //Algorithmic Paradigm: Divide and conquer.
+// Above function can be optimized to O(logn) by calculating power(x, y/2) only once and storing it. 
+        
+            
+        if(n==0){return 1;}
+        
+        double temp = myPow(x, n/2);
+        
+        if(n % 2 == 0){
+            return temp * temp;
+        }
+        else{
+            
+            if(n > 0)
+            return x * temp * temp;
+            else
+                return (temp*temp) / x;
+        }
     }
 }
