61157c800d0e11d0f8e859cd773cec561d60250c
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2022-10-30 12:04:42 +0530

    Time: 1 ms (99.92%), Space: 54.1 MB (73.07%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,14 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        int l = 0, r = s.length-1;
-        recur(s,l,r);
-    }
-    void recur(char[] s, int l, int r){
-        if(l>=r){return;}
-         char temp = s[l]; 
-            s[l] = s[r];
-            s[r] = temp;
-            
-            recur(s, l+1, r-1);
+        int i = 0;
+        int j = s.length - 1;
+        
+        while(i <= j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
+    }
     }
