6f5f4f77861e0d5116e526e1f757924084ebc9da
Ayush Pattnaik <76760708+Ayu10x@users.noreply.github.com>
2022-10-31 22:44:28 +0530

    Time: 7 ms (49.68%), Space: 50.5 MB (49.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int v : nums){
+            res ^= v;
+        }
+        return res;
+        
+    }
+}
